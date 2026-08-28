c1c1f8b4484c94f2005288ba880dd0584d9d48fc
Ezekiel Emil Mjema <mjema.ezekiel@gmail.com>
2026-08-28 07:30:43 +0200

    migration header: the canonical license header

--- migrations/Version20260828060000.php
+++ migrations/Version20260828060000.php
@@ -1,13 +1,15 @@
 <?php
 
-/*
- * Copyright (C) 2026 Uhifadhi Labs
- *
- * This file is part of uhifadhi and is proprietary software.
- * Unauthorized copying, modification or distribution is prohibited.
- */
-
 declare(strict_types=1);
+
+/*
+ * This file is part of uhifadhi.
+ *
+ * (c) Ezekiel Mjema <https://github.com/eemjema>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace DoctrineMigrations;
 
